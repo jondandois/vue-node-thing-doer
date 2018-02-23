804b267a11e46f37bbee16f49e18830a6ca0b3e5
Jonathan <jon.dandois@gmail.com>
2018-02-22 21:51:47 -0500

    getting basic client and server side running

--- application/src/App.vue
+++ application/src/App.vue
@@ -1,23 +1,32 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <v-container>
+      <router-view/>
+    </v-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'app'
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  @import "./assets/styles";
+
+  body {
+    background: url('./assets/images/background.jpg') no-repeat center center fixed;
+    &:after {
+      content: '';
+      position: fixed;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      background-color: $background-tint;
+      opacity: .3;
+      z-index: -1;
+    }
+  }
 </style>
